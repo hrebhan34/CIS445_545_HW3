cdbdd5beb0ceaa2de0abe5884f2e50672bc8ce15
Hannah Rebhan <hrebhan@mail.bradley.edu>
2022-11-06 21:16:27 -0600

    save changes in my html contact form

--- <contact>_profile.js
+++ <contact>_profile.js
@@ -1,17 +1,9 @@
 <form onsubmit="return false"></form>
-let genderValue = male.checked ? "male" : "female"
-function getGender() {
-    return document.querySelector('input[name="gender"]:checked')
-   }
+
    document.addEventListener("DOMContentLoaded", function() {
     fields.firstName = document.getElementById('firstName');
     fields.lastName = document.getElementById('lastName');
     fields.email = document.getElementById('email');
-    fields.address = document.getElementById('address');
-    fields.houseNumber = document.getElementById('houseNumber');
-    fields.country = document.getElementById('country');
-    fields.password = document.getElementById('password');
-    fields.passwordCheck = document.getElementById('passwordCheck');
     fields.newsletter = document.getElementById('newsletter');
     fields.question = document.getElementById('question');
    })
@@ -26,12 +18,7 @@ function getGender() {
     let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
     return regex.test(String(email).toLowerCase());
    }
-   function isPasswordValid(password) {
-    if (password.length > 5) {
-    return true;
-    }
-    return false
-   }
+ 
    function fieldValidation(field, validationFunction) {
     if (field == null) return false;
    
@@ -49,33 +36,16 @@ function getGender() {
     
     valid &= fieldValidation(fields.firstName, isNotEmpty);
     valid &= fieldValidation(fields.lastName, isNotEmpty);
-    valid &= fieldValidation(fields.gender, isNotEmpty);
-    valid &= fieldValidation(fields.address, isNotEmpty);
-    valid &= fieldValidation(fields.country, isNotEmpty);
     valid &= fieldValidation(fields.email, isEmail);
-    valid &= fieldValidation(fields.houseNumber, isNumber);
-    valid &= fieldValidation(fields.password, isPasswordValid);
-    valid &= fieldValidation(fields.passwordCheck, isPasswordValid);
     valid &= fieldValidation(fields.question, isNotEmpty);
-    valid &= arePasswordsEqual();
    
     return valid;
    }
-   function arePasswordsEqual() {
-    if (fields.password.value == fields.passwordCheck.value) {
-    field.password.className = 'placeholderRed';
-    field.passwordCheck.className = 'placeholderRed';
-    return true;
-    }
-    return false
-   }
+
    class User {
-    constructor(firstName, lastName, gender, address, country, email, newsletter, question) {
+    constructor(firstName, lastName, email, newsletter, question) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.gender = gender;
-    this.address = address;
-    this.country = country;
     this.email = email;
     this.newsletter = newsletter;
     this.question = question;
@@ -83,11 +53,9 @@ function getGender() {
    }
 
    function sendContact(){
-    fields.gender = getGender();
 
     if (isValid()){
-        let usr = new User(firstName.value, lastName.value, fields.gender.value,
-            address.value, country.value, email.value, newsletter.checked);
+        let usr = new User(firstName.value, lastName.value, email.value, newsletter.checked);
 
             alert('${usr.firstName} thanks for registering.')
         }
